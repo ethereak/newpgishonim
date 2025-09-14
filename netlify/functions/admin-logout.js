0a79fab3c8cd90e240a6641ff3f84b2e89ca3609
Cojones <43956463+ethereak@users.noreply.github.com>
2025-09-14 13:55:11 +0300

    Strip repo to basic slip generator

--- netlify/functions/admin-logout.js
+++
@@ -1,6 +0,0 @@
-const { ok, serverError, clearSessionCookie } = require("./_utils.js");
-exports.handler = async () => {
-  try {
-    return { statusCode: 200, headers: { "set-cookie": clearSessionCookie(), "content-type": "application/json" }, body: JSON.stringify({ ok: true }) };
-  } catch (e) { return serverError(e); }
-};
